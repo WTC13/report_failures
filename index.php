3075fce6ed1d6cf100c7fcb93a67f4eadbd74899
wtc <wendell.timoteoc@gmail.com>
2025-10-16 11:24:12 -0300

    init project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../public/css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <title>Reporte de Falhas</title>
+    </head>
+    <body style="background: linear-gradient(to right, white, lightblue, #3090db);">
+        <div class="container">
+            <div class="d-flex justify-content-center flex-column vh-100">
+                <div class="card shadow bg-light">      
+                    <h2 class="text-primary d-flex justify-content-center p-3 mb-3 mt-3">Reporte de Falhas</h2> 
+                    <div class="p-3 mb-4 rounded">     
+                        <a href="./views/forms.php" class="d-block p-3 mb-3 btn btn-primary text-light fw-bold text-decoration-none">Faça seu reporte</a>               
+                        <a href="./views/reports_dev.php" class="d-block p-3 btn btn-success text-light fw-bold text-decoration-none">Acompanhe seu reporte</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+</html>
