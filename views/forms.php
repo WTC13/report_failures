ded5c8813883393c7103385046d9acefa6026fb0
wtc <wendell.timoteoc@gmail.com>
2025-10-16 12:25:50 -0300

    include forms

--- views/forms.php
+++ views/forms.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de reporte</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../public/css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <title>Cadastro de reporte</title>
+    </head>
+    <body>
+        <div class="container">
+            <h2 class="text-primary">Cadastre seu Formulário</h2>
+            <form action="cadastrar.php">
+                <div class="form-group">
+                    <label for="#">N° de protocolo:</label>
+                    <input type="text" class="form-control" name="protocolo" placeholder="Insira o protocolo">
+                    <small>Digite o número de protocolo do reporte de falhas</small>
+                </div>
+            </form>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        </div>
+    </body>
 </html>
